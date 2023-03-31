00243f9ac4d83a55688a996b7c5bbd5d9c65cd3b
dreinicke27 <danicareinicke@gmail.com>
2023-03-31 15:07:44 -0700

    wrote tests to verify adding movie to watched

--- tests/test_person.py
+++ tests/test_person.py
@@ -3,15 +3,15 @@ from viewing_party.person import Person
 
 def test_create_person():
     # Arrange / Act
-    kendall = Person("Kendall", ["Nancy"], [])
+    kendall = Person("Kendall", ["Nancy"], [], [])
 
     # Assert
     assert isinstance(kendall, Person)
 
 def test_person_name_is_set_correctly():
     # Arrange / Act
-    nancy = Person("Nancy", ["Kendall"], [])
-    danica = Person("Danica", ["Ping"], [])
+    nancy = Person("Nancy", ["Kendall"], [], [])
+    danica = Person("Danica", ["Ping"], [], [])
 
     # Assert
     assert nancy.name == "Nancy"
@@ -19,14 +19,14 @@ def test_person_name_is_set_correctly():
 
 def test_person_friends_list_is_set_correctly():
     # Arrange / Act
-    ana = Person("Ana", ["Ariel"], [])
+    ana = Person("Ana", ["Ariel"], [], [])
 
     # Assert
     assert ana.friends == ["Ariel"]
 
 def test_friend_added_to_person():
     # Arrange
-    laura = Person("Laura", ["Sage"], [])
+    laura = Person("Laura", ["Sage"], [], [])
 
     # Act
     laura.add_friend("Elizabeth")
@@ -36,7 +36,7 @@ def test_friend_added_to_person():
 
 def test_no_duplicate_friends_added():
     # Arrange
-    moyo = Person("Moyo", ["Sarah"], [])
+    moyo = Person("Moyo", ["Sarah"], [], [])
 
     # Act
     moyo.add_friend("Sarah")
@@ -46,7 +46,7 @@ def test_no_duplicate_friends_added():
 
 def test_new_movie_added_to_watchlist():
     # Arrange
-    danica = Person("Danica", ["Sage"], ["The Princess Bride"])
+    danica = Person("Danica", ["Sage"], ["The Princess Bride"],[])
 
     # Act
     danica.add_to_watchlist("What We Do In the Shadows")
@@ -57,7 +57,7 @@ def test_new_movie_added_to_watchlist():
 
 def test_no_duplicate_added_to_watchlist():
     # Arrange
-    danica = Person("Danica", ["Sage"], ["The Princess Bride"])
+    danica = Person("Danica", ["Sage"], ["The Princess Bride"],[])
 
     # Act
     danica.add_to_watchlist("The Princess Bride")
@@ -65,3 +65,37 @@ def test_no_duplicate_added_to_watchlist():
     # Assert
     assert danica.watchlist == ["The Princess Bride"]
     assert len(danica.watchlist) == 1
+
+def test_add_movie_from_watchlist_to_watched():
+    # Arrange
+    sage = Person("Sage", ["Danica"], ["What We Do in the Shadows"], ["The Princess Bride"])
+
+    # Act 
+    sage.watch_movie("What We Do in the Shadows")
+
+    # Assert
+    assert sage.watchlist == []
+    assert sage.watched == ["The Princess Bride", "What We Do in the Shadows"]
+
+def test_add_new_movie_not_from_watchlist():
+    # Arrange
+    sage = Person("Sage", ["Danica"], ["What We Do in the Shadows"], ["The Princess Bride"])
+
+    # Act 
+    sage.watch_movie("Cars")
+
+    # Assert
+    assert sage.watchlist == ["What We Do in the Shadows"]
+    assert sage.watched == ["The Princess Bride", "Cars"]
+
+
+def test_no_duplicates_in_watched():
+    # Arrange
+    sage = Person("Sage", ["Danica"], ["What We Do in the Shadows"], ["The Princess Bride"])
+
+    # Act 
+    sage.watch_movie("The Princess Bride")
+
+    # Assert
+    assert sage.watchlist == ["What We Do in the Shadows"]
+    assert sage.watched == ["The Princess Bride"]  

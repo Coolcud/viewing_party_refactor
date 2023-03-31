74555b87e4ac063b7b5d37985118347e6088d993
dreinicke27 <danicareinicke@gmail.com>
2023-03-31 15:08:16 -0700

    added watched parameter and function to watch a movie

--- viewing_party/person.py
+++ viewing_party/person.py
@@ -1,8 +1,9 @@
 class Person:
-    def __init__(self, name, friends, watchlist):
+    def __init__(self, name, friends, watchlist, watched):
         self.name = name
         self.friends = friends
         self.watchlist = watchlist
+        self.watched = watched
     
     def add_friend(self, friend):
         if friend not in self.friends:
@@ -11,3 +12,10 @@ class Person:
     def add_to_watchlist(self, new_movie):
         if new_movie not in self.watchlist:
             self.watchlist.append(new_movie)
+    
+    def watch_movie(self, movie):
+        if movie in self.watchlist:
+            self.watchlist.remove(movie) 
+
+        if movie not in self.watched:
+            self.watched.append(movie)  

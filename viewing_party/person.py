8cd491dfbddf8cdd6db7bedc2e42ebb81fe23f41
Sage Choi <coolcuds@gmail.com>
2023-03-31 14:57:43 -0700

    Added watchlist parameter and add to watchlist function

--- viewing_party/person.py
+++ viewing_party/person.py
@@ -1,8 +1,13 @@
 class Person:
-    def __init__(self, name, friends):
+    def __init__(self, name, friends, watchlist):
         self.name = name
         self.friends = friends
+        self.watchlist = watchlist
     
     def add_friend(self, friend):
         if friend not in self.friends:
             self.friends.append(friend)
+
+    def add_to_watchlist(self, new_movie):
+        if new_movie not in self.watchlist:
+            self.watchlist.append(new_movie)
